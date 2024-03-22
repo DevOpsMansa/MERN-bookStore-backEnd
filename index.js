f79adde47bf01af9c0ebae277c0dde76191d8cfe
Eric (Mansa) Marah <devopsmansa@gmail.com>
2024-03-22 15:56:15 -0500

    Third commit: connected server to mongodb

--- index.js
+++ index.js
@@ -7,13 +7,43 @@ const cors = require('cors')
 
 //middleware to make connection to frontend site
 app.use(cors());
-app.send(express.json());
+app.use(express.json());
 
 
 
 app.get('/', (req, res) => {
  res.send('Hello World!')
 })
+
+//mongodb configuration
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://bookStoreUser:mpBQ7OULS9nJQeuD@dbapp.xenzuty.mongodb.net/?retryWrites=true&w=majority&appName=dbApp";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.listen(port, () => {
