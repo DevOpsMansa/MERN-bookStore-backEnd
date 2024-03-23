496b365d4fffde2053d3b31c2814c59cf27a72d4
Eric (Mansa) Marah <devopsmansa@gmail.com>
2024-03-23 09:11:28 -0500

    Forth commit: Created CRUD operartion on the server

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-//imports
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
-
-//middleware to make connection to frontend site
+// middlewear
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-app.get('/', (req, res) => {
- res.send('Hello World!')
-})
-
-//mongodb configuration
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://bookStoreUser:mpBQ7OULS9nJQeuD@dbapp.xenzuty.mongodb.net/?retryWrites=true&w=majority&appName=dbApp";
+// mongodb confiq here
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://bookStoreUser:mpBQ7OULS9nJQeuD@dbapp.xenzuty.mongodb.net/?retryWrites=true&w=majority&appName=dbApp";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,7 +22,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -35,17 +30,82 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    const bookCollections = client.db("BookInventory").collection("Books");
+
+    // insert a book to db: Post Method
+    app.post("/upload-book", async (req, res) => {
+      const data = req.body;
+      // console.log(data);
+      const result = await bookCollections.insertOne(data);
+      res.send(result);
+    });
+
+    // // get all books from db
+    // app.get("/all-books", async (req, res) => {
+    //     const books = bookCollections.find();
+    //     const result = await books.toArray();
+    //     res.send(result)
+    // })
+
+    // get all books & find by a category from db: get method
+    app.get("/all-books", async (req, res) => {
+      let query = {};
+      if (req.query?.category) {
+        query = { category: req.query.category };
+      }
+      const result = await bookCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    // update  books method in db: patch method
+    app.patch("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const updateBookData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          ...updateBookData,
+        },
+      };
+      const options = { upsert: true };
+
+      // update now
+      const result = await bookCollections.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // delete a item from db: Delete method
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollections.deleteOne(filter);
+      res.send(result);
+    });
+
+    // get a single book data
+    app.get("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollections.findOne(filter);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
 app.listen(port, () => {
- console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
